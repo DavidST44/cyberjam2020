35a2b0613a6cc6e540c1abff7e0ea4db4b453f26
Ninjapig26 <ninjapig26@gmail.com>
2020-02-01 19:21:50 +1100

    PavlovVR
    Finished up Pavlov VR

---
+++ OculusBase/Assets/Grace.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Grace : MonoBehaviour
+{
+    public SoundRandomizer[] soundRando;
+    public int graceRounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("GraceOver", graceRounds);
+    }
+
+    void GraceOver()
+    {
+        foreach (SoundRandomizer sound in soundRando)
+        {
+            sound.enabled = true;
+        }
+    }
+}
