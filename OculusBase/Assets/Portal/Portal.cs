7eae3ce2f764bd0355a7e72900bef1b8cbe36509
Ninjapig26 <ninjapig26@gmail.com>
2020-02-01 17:25:10 +1100

    Portal Working
    Drag portal size and transport to new world

---
+++ OculusBase/Assets/Portal/Portal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject player;
+    public string level;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player && transform.localScale.x >= 0.8f)
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
